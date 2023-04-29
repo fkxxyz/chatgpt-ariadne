966d01df4ddf721de6867f7abd7b0e99e5695184
fkxxyz <fkxxyz@163.com>
2023-04-30 04:08:52 +0800

    feat: auto agree master friend request add

--- modules/friend.py
+++ modules/friend.py
@@ -33,8 +33,10 @@ async def new_friend_request_listener(app: Ariadne, event: NewFriendRequestEvent
         f"昵称： {event.nickname}\n"
         f"附加消息： {event.message}"
     )
-    await master_cor
-    return
+
+    if event.supplicant not in instance.config.accounts_map[app.account].masters:
+        await master_cor
+        return
     user_info = UserInfo(
         user_id=str(event.supplicant),  # 用户QQ号
         user_nickname=event.nickname,  # 用户昵称
